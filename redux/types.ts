cbbb817a6b5d62e719afa60671db88d0140a5465
Daniel Campos <danielgcampos.04@gmail.com>
2021-07-15 19:25:05 -0300

    node api

--- redux/types.ts
+++ redux/types.ts
@@ -1,12 +1,12 @@
 //images types
 const GET_POSTS: string = 'GET_POSTS';
-const URL_POSTS: string = 'http://localhost:3002/post/';
+const URL_POSTS: string = 'https://coscu-node.herokuapp.com/post/';
 //user types
 const CREATE_USER: string = 'CREATE_USER';
 const LOG_OUT: string = 'LOG_OUT';
 const GET_USER: string = 'GET_USER';
 const EDIT_USER: string = 'EDIT_USER';
-const URL_USER: string = 'http://localhost:3002/users/';
+const URL_USER: string = 'https://coscu-node.herokuapp.com/users/';
 
 export { 
     GET_POSTS,
