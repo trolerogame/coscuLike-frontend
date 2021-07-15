c95018133d16cf4ca01a961e1b88c5d64e867a99
Daniel Campos <danielgcampos.04@gmail.com>
2021-07-15 20:05:47 -0300

    arreglando head

--- components/head.tsx
+++ components/head.tsx
@@ -8,14 +8,13 @@ const head = ({title,children}: HeadInterface) => {
   return (
     <div>
       <Head>
-        <title>{title}</title>
-        <html lang={"es"} />
-        <meta name="description" content="Author:Daniel Campos"></meta>
+      <meta name="description" content="Author:Daniel Campos"></meta>
         <meta
           name="viewport"
           content="width=device-width, height=device-height, initial-scale=1.0, minimum-scale=1.0"
         ></meta>
-        
+        <title>{title}</title>
+        <html lang={"es"} />
       </Head>
       {children}
     </div>
